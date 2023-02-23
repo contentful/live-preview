0872534663dc2daf7845c8e473c7a5db14375d37
aodhagan-cf <109096340+aodhagan-cf@users.noreply.github.com>
2023-02-23 16:01:59 +0000

    fix: adding types to build [TOL-909] (#6)
    * fix: adding types to build [TOL-909]
    
    * chore: removing types from src []
    
    * chore: adding dts vite plugin []
    
    * chore: removing release version as it determined by semantic []

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
@@ -19,5 +20,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(), dts()],
 });
