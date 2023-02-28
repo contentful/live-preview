af0bb861854ec60dfbf976b0e5621dd657407224
MayaGillilanContentful <101652314+MayaGillilanContentful@users.noreply.github.com>
2023-02-28 10:36:47 +0100

    feat: send tagged field info on click (#12)

--- src/features/tagging/getLivePreviewProps.ts
+++ src/features/tagging/getLivePreviewProps.ts
@@ -4,10 +4,26 @@ export type LivePreviewProps = {
   locale: string | null | undefined;
 };
 
+enum TagAttributes {
+  FIELD_ID = 'data-contentful-field-id',
+  ENTRY_ID = 'data-contentful-entry-id',
+  LOCALE = 'data-contentful-locale',
+}
+
 export const getLivePreviewProps = ({ fieldId, entryId, locale }: LivePreviewProps) => {
   return {
-    'data-contentful-field-id': fieldId,
-    'data-contentful-entry-id': entryId,
-    'data-contentful-locale': locale,
+    [TagAttributes.FIELD_ID]: fieldId,
+    [TagAttributes.ENTRY_ID]: entryId,
+    [TagAttributes.LOCALE]: locale,
+    onClick: () =>
+      window.top?.postMessage(
+        {
+          fieldId,
+          entryId,
+          locale,
+        },
+        //todo: check if there is any security risk with this
+        '*'
+      ),
   };
 };
