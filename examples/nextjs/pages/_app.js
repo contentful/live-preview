8bd4dddcc906f048a2f1cf349eb2b93930e58656
Yves Rijckaert <hi@yvesrijckaert.com>
2023-05-17 12:46:27 +0200

    feat: disable when not in draft mode

--- examples/nextjs/pages/_app.js
+++ examples/nextjs/pages/_app.js
@@ -3,7 +3,7 @@ import "@contentful/live-preview/style.css";
 
 function App({ Component, pageProps }) {
   return (
-    <ContentfulLivePreviewProvider locale="en-US" debugMode={true}>
+    <ContentfulLivePreviewProvider locale="en-US" enableInspectorMode={pageProps.draftMode} enableLiveUpdates={pageProps.draftMode}>
       <Component {...pageProps} />
     </ContentfulLivePreviewProvider>
   );
