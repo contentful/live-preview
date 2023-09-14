73b8d0568d0e5b514450ef8793e35da649ce9e05
Yuri Mazursky <colomolo@users.noreply.github.com>
2023-09-14 19:12:58 +0100

    feat: add origins for editor postMessage

--- packages/live-preview-sdk/src/constants.ts
+++ packages/live-preview-sdk/src/constants.ts
@@ -12,3 +12,5 @@ export const MAX_DEPTH = 10;
 
 export const LIVE_PREVIEW_EDITOR_SOURCE = 'live-preview-editor' as const;
 export const LIVE_PREVIEW_SDK_SOURCE = 'live-preview-sdk' as const;
+
+export const CONTENTFUL_ORIGINS = ['https://app.contentful.com', 'https://app.eu.contentful.com'];
