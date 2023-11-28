72423a60f53492b52610fc3029c9c9efd0045eb6
Chris Helgert <chrishelgert@users.noreply.github.com>
2023-11-28 12:16:04 +0100

    fix(validation): warn about invalid query usage on live updates subscription [] (#450)
    * fix(validation): warn about invalid query usage on live updates subscription []
    
    * fix(validation): prevent infinite loop during validation []
    
    * chore(validation): parse DocumentNode from query string []

--- packages/live-preview-sdk/src/index.ts
+++ packages/live-preview-sdk/src/index.ts
@@ -47,7 +47,7 @@ export interface ContentfulSubscribeConfig {
   data: Argument;
   locale?: string;
   callback: SubscribeCallback;
-  query?: DocumentNode;
+  query?: DocumentNode | string;
 }
 
 export class ContentfulLivePreview {
