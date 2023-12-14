709e8360faadca9c1d3fa4eb40178e7b1c9253af
Yves Rijckaert <hi@yvesrijckaert.com>
2023-12-14 10:37:27 +0100

    feat: implement content source maps

---
+++ packages/live-preview-sdk/src/csm/index.ts
@@ -0,0 +1,104 @@
+import { vercelStegaEncode } from '@vercel/stega';
+import { debug } from '../helpers';
+import jsonPointer from 'json-pointer';
+
+type Source = {
+  field: number;
+  locale: number;
+} & ({ entry: number } | { asset: number });
+
+interface EntitySource {
+  space: number;
+  environment: number;
+  id: string;
+}
+
+type Mappings = Record<string, { source: Source }>;
+
+interface IContentSourceMaps {
+  version: number;
+  spaces: string[];
+  environments: string[];
+  fields: string[];
+  locales: string[];
+  entries: EntitySource[];
+  assets: EntitySource[];
+  mappings: Mappings;
+}
+
+interface GraphQLResponse {
+  data: any;
+  extensions: {
+    contentSourceMaps: IContentSourceMaps;
+  };
+}
+
+export class ContentSourceMaps {
+  private isUrlOrIsoDate(value: string) {
+    // Regular expression for URL validation
+    const urlRegex = /^(http|https):\/\/[^ "]+$/;
+    // Regular expression for ISO 8601 date validation
+    const isoDateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(.\d+)?([+-]\d{2}:\d{2}|Z)?$/;
+
+    // Check if the string matches URL or ISO 8601 date format
+    return urlRegex.test(value) || isoDateRegex.test(value);
+  }
+
+  private getHref(
+    source: Source,
+    entries: EntitySource[],
+    assets: EntitySource[],
+    spaces: string[],
+    environments: string[],
+    fields: string[],
+    locales: string[]
+  ): string | null {
+    const isEntry = 'entry' in source;
+    const entity = isEntry ? entries[source.entry] : assets[source.asset];
+    if (!entity) return null;
+
+    const space = spaces[entity.space];
+    const environment = environments[entity.environment];
+    const entityId = entity.id;
+    const field = fields[source.field];
+    const locale = locales[source.locale];
+
+    const basePath = `https://app.contentful.com/spaces/${space}/environments/${environment}`;
+    const entityType = isEntry ? 'entries' : 'assets';
+    return `${basePath}/${entityType}/${entityId}/?focusedField=${field}&focusedLocale=${locale}`;
+  }
+
+  encodeSourceMap(graphqlResponse: GraphQLResponse): GraphQLResponse {
+    if (
+      !graphqlResponse ||
+      !graphqlResponse.extensions ||
+      !graphqlResponse.extensions.contentSourceMaps
+    ) {
+      debug.error('GraphQL response does not contain Content Source Maps information.');
+      return graphqlResponse;
+    }
+    const { spaces, environments, fields, locales, entries, assets, mappings } =
+      graphqlResponse.extensions.contentSourceMaps;
+    const data = graphqlResponse.data;
+
+    for (const pointer in mappings) {
+      const { source } = mappings[pointer];
+      const href = this.getHref(source, entries, assets, spaces, environments, fields, locales);
+
+      if (href && jsonPointer.has(data, pointer)) {
+        const currentValue = jsonPointer.get(data, pointer);
+
+        if (!this.isUrlOrIsoDate(currentValue)) {
+          const encodedValue = vercelStegaEncode({
+            origin: 'contentful.com',
+            href,
+          });
+          jsonPointer.set(data, pointer, `${encodedValue}${currentValue}`);
+        }
+      } else {
+        debug.error(`Pointer ${pointer} not found in GraphQL data or href could not be generated.`);
+      }
+    }
+    return graphqlResponse;
+  }
+}
