2b113dc400888c8146ce4cca2edcfcbcf81c9322
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2024-01-19 17:41:14 +0100

    fix: fix linting setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,27 @@
 module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
-    require.resolve('@contentful/eslint-config-extension'),
-    require.resolve('@contentful/eslint-config-extension/react.js'),
-    require.resolve('@contentful/eslint-config-extension/typescript.js'),
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
-  plugins: ['eslint-plugin-import-helpers'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'eslint-plugin-import-helpers'],
   rules: {
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     'import-helpers/order-imports': [
       'warn',
       {
@@ -14,6 +30,9 @@ module.exports = {
         alphabetize: { order: 'asc', ignoreCase: true },
       },
     ],
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: { version: 'detect' },
   },
 };
