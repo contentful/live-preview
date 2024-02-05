9cc336aa1a5517117f6f6834693d81a87b842525
Aodhagan Murphy <aodhagan.murphy@contentful.com>
2024-02-05 13:14:28 +0000

    fix: json csm [TOL-1788]

--- packages/live-preview-sdk/src/index.ts
+++ packages/live-preview-sdk/src/index.ts
@@ -78,7 +78,7 @@ export class ContentfulLivePreview {
   static init(config: ContentfulLivePreviewInitConfig): Promise<InspectorMode | null> | undefined {
     if (typeof config !== 'object' || !config?.locale) {
       throw new Error(
-        "Init function have to be called with a locale configuration (for example: `ContentfulLivePreview.init({ locale: 'en-US'})`)"
+        "Init function have to be called with a locale configuration (for example: `ContentfulLivePreview.init({ locale: 'en-US'})`)",
       );
     }
 
@@ -169,16 +169,16 @@ export class ContentfulLivePreview {
           {
             action: LivePreviewPostMessageMethods.URL_CHANGED,
             taggedElementCount: document.querySelectorAll(
-              `[${InspectorModeDataAttributes.ENTRY_ID}]`
+              `[${InspectorModeDataAttributes.ENTRY_ID}]`,
             ).length,
           } as UrlChangedMessage,
-          this.targetOrigin
+          this.targetOrigin,
         );
       });
 
       // tell the editor that there's a SDK
       const taggedElementCount = document.querySelectorAll(
-        `[${InspectorModeDataAttributes.ENTRY_ID}]`
+        `[${InspectorModeDataAttributes.ENTRY_ID}]`,
       ).length;
       sendMessageToEditor(
         LivePreviewPostMessageMethods.CONNECTED,
@@ -191,7 +191,7 @@ export class ContentfulLivePreview {
           isInspectorEnabled: this.inspectorModeEnabled,
           isLiveUpdatesEnabled: this.liveUpdatesEnabled,
         } as ConnectedMessage,
-        this.targetOrigin
+        this.targetOrigin,
       );
 
       // all set up - ready to go
@@ -204,12 +204,12 @@ export class ContentfulLivePreview {
   static subscribe(config: ContentfulSubscribeConfig): VoidFunction;
   static subscribe(
     event: 'save',
-    config: Pick<ContentfulSubscribeConfig, 'callback'>
+    config: Pick<ContentfulSubscribeConfig, 'callback'>,
   ): VoidFunction;
   static subscribe(event: 'edit', config: ContentfulSubscribeConfig): VoidFunction;
   static subscribe(
     configOrEvent: 'save' | 'edit' | ContentfulSubscribeConfig,
-    config?: ContentfulSubscribeConfig | Pick<ContentfulSubscribeConfig, 'callback'>
+    config?: ContentfulSubscribeConfig | Pick<ContentfulSubscribeConfig, 'callback'>,
   ): VoidFunction {
     if (!this.liveUpdatesEnabled) {
       return () => {
@@ -223,7 +223,7 @@ export class ContentfulLivePreview {
     if (event === 'save') {
       if (!this.saveEvent) {
         throw new Error(
-          'Save event is not initialized, please call `ContentfulLivePreview.init()` first.'
+          'Save event is not initialized, please call `ContentfulLivePreview.init()` first.',
         );
       }
       return this.saveEvent.subscribe(subscribeConfig.callback);
@@ -231,7 +231,7 @@ export class ContentfulLivePreview {
 
     if (!this.liveUpdates) {
       throw new Error(
-        'Live updates are not initialized, please call `ContentfulLivePreview.init()` first.'
+        'Live updates are not initialized, please call `ContentfulLivePreview.init()` first.',
       );
     }
 
@@ -282,7 +282,7 @@ export class ContentfulLivePreview {
         props.fieldId,
         (props as LivePreviewAssetProps).assetId,
         props.locale || this.locale,
-        this.targetOrigin
+        this.targetOrigin,
       );
       return;
     }
@@ -292,7 +292,7 @@ export class ContentfulLivePreview {
         props.fieldId,
         (props as LivePreviewEntryProps).entryId,
         props.locale || this.locale,
-        this.targetOrigin
+        this.targetOrigin,
       );
       return;
     }
@@ -317,3 +317,4 @@ export class ContentfulLivePreview {
 
 export { LIVE_PREVIEW_EDITOR_SOURCE, LIVE_PREVIEW_SDK_SOURCE } from './constants';
 export * from './messages';
+export * from './csm';
