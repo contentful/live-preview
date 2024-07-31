2460895216647095535addf4d4ea7131aaf896f8
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2024-07-31 13:56:07 +0200

    Revert "feat: setup swc [TOL-1167] (#808)"
    This reverts commit 4486a7b648ddd2dda3dc7a989f7b08d991a56a7e.

---
+++ packages/live-preview-sdk/vite.config.ts
@@ -0,0 +1,43 @@
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import { configDefaults } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    exclude: [
+      ...configDefaults.exclude,
+      'src/rest/__tests__/constants.ts',
+      'src/rest/__tests__/utils.ts',
+      'src/rest/__tests__/fixtures/data.ts',
+    ],
+  },
+  build: {
+    sourcemap: true,
+    outDir: './dist',
+    lib: {
+      entry: {
+        index: resolve(__dirname, 'src/index.ts'),
+        react: resolve(__dirname, 'src/react.tsx'),
+      },
+      formats: ['cjs', 'es'],
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
+  plugins: [
+    react(),
+    dts({
+      entryRoot: 'src',
+      exclude: ['**/__tests__/**'],
+    }),
+  ],
+});
