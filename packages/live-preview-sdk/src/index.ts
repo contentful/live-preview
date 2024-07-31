cab995d682d66e5d84a287f713f38a0518210306
Chris Helgert <chrishelgert@users.noreply.github.com>
2024-07-31 09:52:07 +0200

    fix(import/export): import version correctly on the remaining place [] (#823)

--- packages/live-preview-sdk/src/index.ts
+++ packages/live-preview-sdk/src/index.ts
@@ -5,7 +5,7 @@ import {
 } from '@contentful/content-source-maps';
 import { type DocumentNode } from 'graphql';
 
-import { version } from '../package.json';
+import pkg from '../package.json';
 import {
   debug,
   isInsideIframe,
@@ -39,7 +39,7 @@ import type {
 
 export type { LivePreviewAssetProps, LivePreviewEntryProps, LivePreviewProps };
 
-export const VERSION = version;
+export const VERSION = pkg.version;
 
 const DEFAULT_ALLOWED_ORIGINS = [
   'https://app.contentful.com',
