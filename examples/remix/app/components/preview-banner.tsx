886ad4ef977b344f6fd0cc67390d9114277eb073
Eric Schmidt <e.ryan.schmidt@gmail.com>
2024-12-09 14:40:23 -0700

    fix: add proper DOM nesting for Remix example preview banner.

--- examples/remix/app/components/preview-banner.tsx
+++ examples/remix/app/components/preview-banner.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export function PreviewBanner() {
   return (
-    <p>
-      You&apos;re in <strong>preview mode</strong> (DRAFT content from Contentful served)
+    <>
+      <p>You're in <strong>preview mode</strong> (DRAFT content from Contentful served)</p>
       <form action="/api/exit-preview" method="post">
         <button type="submit">Exit Preview Mode</button>
       </form>
-    </p>
+    </>
   );
 }
