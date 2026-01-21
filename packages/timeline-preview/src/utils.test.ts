59faf963dc2b0028682729bbf4a9f851cdc9be52
MayaGillilan <maya.gillilan@contentful.com>
2026-01-21 15:13:41 +0100

    fix: encode timeline token delimiter [ZEND-7495]

--- packages/timeline-preview/src/utils.test.ts
+++ packages/timeline-preview/src/utils.test.ts
@@ -6,17 +6,17 @@ describe('utils', () => {
   describe('buildTimelinePreviewToken', () => {
     it('should build a timeline preview token (with releaseId)', () => {
       const token = buildTimelinePreviewToken({ releaseId: '123' });
-      expect(token).toBe('123;');
+      expect(token).toBe(encodeURIComponent('123;'));
     });
 
     it('should build a timeline preview token (with timestamp)', () => {
       const token = buildTimelinePreviewToken({ timestamp: '2021-01-01' });
-      expect(token).toBe(';2021-01-01');
+      expect(token).toBe(encodeURIComponent(';2021-01-01'));
     });
 
     it('should build a timeline preview token (with releaseId and timestamp)', () => {
       const token = buildTimelinePreviewToken({ releaseId: '123', timestamp: '2021-01-01' });
-      expect(token).toBe('123;2021-01-01');
+      expect(token).toBe(encodeURIComponent('123;2021-01-01'));
     });
 
     it('should throw an error if no releaseId or timestamp is provided', () => {
@@ -28,19 +28,19 @@ describe('utils', () => {
 
   describe('parseTimelinePreviewToken', () => {
     it('should parse a timeline preview token (with releaseId)', () => {
-      const token = '123;';
+      const token = encodeURIComponent('123;');
       const parsedToken = parseTimelinePreviewToken(token);
       expect(parsedToken).toEqual({ releaseId: '123', timestamp: undefined });
     });
 
     it('should parse a timeline preview token (with timestamp)', () => {
-      const token = ';2021-01-01';
+      const token = encodeURIComponent(';2021-01-01');
       const parsedToken = parseTimelinePreviewToken(token);
       expect(parsedToken).toEqual({ releaseId: undefined, timestamp: '2021-01-01' });
     });
 
     it('should parse a timeline preview token (with releaseId and timestamp)', () => {
-      const token = '123;2021-01-01';
+      const token = encodeURIComponent('123;2021-01-01');
       const parsedToken = parseTimelinePreviewToken(token);
       expect(parsedToken).toEqual({ releaseId: '123', timestamp: '2021-01-01' });
     });
